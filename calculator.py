1c39461e1dd466c54093aa6f02f9632309b0e56b
MidnightKaze <136393920+MidnightKaze@users.noreply.github.com>
2025-01-09 01:53:06 +0000

    All the pieces are coming together :) Taking a small break.

--- calculator.py
+++ calculator.py
@@ -1,32 +1,64 @@
+import math
+
 def decimal_to_binary(num):
-    num = int(num)
-    if num == 0: return 00000000
-    # Some decimal to binary magic
+    # The binary for 0 is 0, so obviously it should just return 0.
+    if num == 0: return 0
+    
+    bin = ""
+    while num > 0:
+        # Obtains a remainder after the argument is divided by 2.
+        remain = num % 2
+        # Adds the remainder to whatever the start of the binary string may be.
+        bin = str(remain) + bin
+        # Resets the number to continue the loop, until num is 0.
+        num = num // 2
+
+    return bin
+
 
 def binary_to_decimal(num):
-    num = int(num)
-    num = num.lstrip()    
-    return int(num, 2) # Removes the leading 0s and converts the number into base2 aka binary
-    # Floor it too
-    # Fix it too
+
+    # Removes any leading 0s which is necessary for the conversion to work correctly.
+    num = num.lstrip('0')
+
+    # Added a case check; as when lstrip does it's magic, 0 in any form will automatically turn into an empty string.
+    if num == '':
+        return 0
+        
+    # Floors the number and turns it into a base 2 integer - binary.
+    return math.floor(int(num, 2))
+
 
 def check_if_binary(num):
+    # Checks for an empty string
+    if num == '':
+        return False
     
-    check = "good"
-    
+    # Checks for anything other than a 1 or 0
     for x in num:
-        if x == "abcdefghijklmnopqrtuvwxyz":
-            check = "bad"
-        elif x != "0" or "1":
-            check = "bad"
+        if x not in "10":
+            return False
+    
+    # Gives the all clear for everything to contrinue
+    return True
+
 
 def binary_calculator(bin1, bin2, operator):
     
-    """
-    > Logically would check bin1 and bin2 in order to convert them to the proper dec/bin whatever before ensuing the math
-    > Would also logicaly check if it's binary first, before moving onto decimals
-    > Would also have to check if any input was alpha
-    """
+    # Starts by checking each number to ensure the starting input is actually binary.
+    if check_if_binary(bin1) == False:
+        return "Error"
+    if check_if_binary(bin2) == False:
+        return "Error"
 
-    if bin2 == 0 and operator == '/': # If denominator is 0, return NaN
+    # Else continues by converting the binary values in decimals.
+    num1 = binary_to_decimal(bin1)
+    num2 = binary_to_decimal(bin2)
+
+    # Checks if the denominator of a division problem is 0. Throws an error if it it.
+    # Will integrate it into the actual thing later on.
+    if num2 == 0 and operator == '/':
         return "NaN"
+    
+print(binary_calculator("00001110", "00000000", "/"))
+    
